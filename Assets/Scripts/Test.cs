48bc15a15108f75e27a78fb2b5bc77e0a260c6ec
Colin Williams <colin.williams.dev@gmail.com>
2024-05-30 23:55:30 -0500

    docs: TODO native lib track from h files

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: locate the C or C++ Native Lib from cmake
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
